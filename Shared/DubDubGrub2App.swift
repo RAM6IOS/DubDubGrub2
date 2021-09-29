7524e0b17f36e34c31084acb42cdc362d994494f
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-09-29 16:24:55 +0100

    Initial Commit

---
+++ Shared/DubDubGrub2App.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrub2App.swift
+//  Shared
+//
+//  Created by MAC on 29/9/2021.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrub2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
